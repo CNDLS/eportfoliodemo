232d7c9830b21ab314230b22b9285fff7cbce5d3
Marie Selvanadin <bhindu20@gmail.com>
2011-08-19 13:18:50 -0400

    Adding LDAP authentication and authorization middleware

--- urls.py
+++ urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 )
