d1f8f6361d5c883fbce98cfcff410740658ee343
Bill Garr <wjg8@georgetown.edu>
2011-09-08 18:42:03 -0400

    folders refresh upon folder move. right now, folders come back collapsed.

--- folders/views.py
+++ folders/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response, redirect
 from django.template.loader import render_to_string
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import simplejson
 from django.template import RequestContext
 from django.contrib.auth.models import User
@@ -10,25 +11,11 @@ from eportfoliodemo.folders.forms import FolderForm
 from eportfoliodemo.snippets.template import render_block_to_string
 
 
-def show(request, user_id):
-    requested_user = User.objects.get(pk=user_id)
-    
-    # json = simplejson.dumps(list(folders_in_tree))
-    # return HttpResponse(json, mimetype='application/json')
-    
-    # will use this with jsTree jQuery plugin to manipulate folder relationships
-    folders_in_tree = Folder.tree.get_query_set().filter(owner=requested_user)
-    return render_block_to_string('library/show', 'folder_tree', { 'nodes': folders_in_tree }, context_instance=RequestContext)
-    
-    # return render_to_string('folders/tree.json', { 'tree': folders_in_tree }) #  , context_instance=RequestContext)
-    # return render_to_response('folders/index.html', { 'nodes': folders_in_tree }, context_instance=RequestContext(request))
-
-
 def new(request):
     current_user = User.objects.get(pk=request.user.id)
     folder_form = FolderForm(instance=Folder(owner=current_user))
 
-    return render_to_response('folders/edit.html', { 'folder_form': folder_form }, context_instance=RequestContext(request))
+    return render_to_response('edit.html', { 'folder_form': folder_form }, context_instance=RequestContext(request))
     
     
 
@@ -50,11 +37,21 @@ def update(request):
     # if folder_form.is_valid():
     folder.save()
 
-    return redirect('/library/' + str(folder.owner_id))
+    return redirect('library/' + str(folder.owner_id))
     
     
     
 # response to the user dragging and dropping folders.
-def move(request, dragged_folder_id, drop_folder_id):
+def move(request, dragged_folder_id, drop_target_id):
     dragged_folder = Folder.objects.get(pk=dragged_folder_id)
-    return redirect('/library/' + str(dragged_folder.owner_id))
+    drop_target = Folder.objects.get(pk=drop_target_id)
+    Folder.tree.move_node(dragged_folder, drop_target)
+    
+    folders_owner = User.objects.get(pk=dragged_folder.owner_id)
+    folders_in_tree = Folder.tree.get_query_set().filter(owner=folders_owner)
+    # return render_to_string ('folders/index.html', { 'nodes': folders_in_tree }, context_instance=RequestContext)
+    if request.is_ajax():
+        return render_to_response('folders/index.html', { 'nodes': folders_in_tree })
+    else:
+        return HttpResponseRedirect(request.META['SCRIPT_NAME'] + '/library/' + str(folders_owner.id))
+    
