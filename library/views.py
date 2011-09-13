260cf499b36d92793df7c3bf3634639eb2b03cd5
Bill Garr <wjg8@georgetown.edu>
2011-09-13 13:44:56 -0400

    restoring library/views.py after conflict

---
+++ library/views.py
@@ -0,0 +1,59 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.forms.models import model_to_dict
+from django.http import HttpResponse
+from itertools import chain
+
+from django.contrib.auth.models import User
+from eportfoliodemo.library.models import LibraryState
+from eportfoliodemo.folders.models import Folder
+from eportfoliodemo.usercollections.models import Collection
+from eportfoliodemo.assets.forms import FileUploadForm
+
+from eportfoliodemo.assets.models import Asset, FileType
+
+from eportfoliodemo.settings import MEDIA_ROOT
+
+
+def show(request, user_id):
+    requested_user = User.objects.get(pk=user_id)
+    current_user = User.objects.get(pk=request.user.id)
+    
+    # library_state allows us to return from presentation editing to the library as we left it.
+    library_state, created = LibraryState.objects.get_or_create(owner=requested_user)
+    
+    # get all the folders & assets. 
+    folders_in_tree = Folder.tree.get_query_set().filter(owner=requested_user)
+    assets_in_tree = Asset.tree.get_query_set().filter(author = request.user)
+    items_in_tree = chain(folders_in_tree, assets_in_tree)
+        
+    try:
+        collections = Collection.objects.get(owner=requested_user)
+    except:
+        collections = []
+        
+    current_assets = Asset.objects.filter(author = request.user)
+    
+    
+    file_upload_form = FileUploadForm()
+    if request.POST:
+        file_type = request.FILES['file'].content_type.split('/')[1]
+        asset = Asset()
+        asset.author = request.user
+        asset.name = str.replace(str(request.FILES['file']), MEDIA_ROOT, '')
+        asset_type, created = FileType.objects.get_or_create(name=file_type)
+        asset.file = request.FILES['file']
+        
+        asset.save()
+        asset.filetype.add(asset_type)
+        
+    return render_to_response('library/show.html',
+                    { 'requested_user': requested_user,
+                      'current_user': current_user,
+                      'current_assets': current_assets,
+                      'nodes': items_in_tree,
+                      'collections': collections,
+                      'file_upload_form': file_upload_form
+                    },
+                    context_instance=RequestContext(request))
