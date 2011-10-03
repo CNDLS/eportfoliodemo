a518dd01e28c6cdf881db0f77c803e9896cf8ad6
Marie Selvanadin <bhindu20@gmail.com>
2011-10-02 20:38:37 -0400

    ajax-like file upload

--- assets/views.py
+++ assets/views.py
@@ -4,8 +4,24 @@ from django.template import RequestContext
 from django.core import serializers
 
 from django.contrib.auth.models import User
-from eportfoliodemo.assets.models import Asset, CustomMetaData
+from eportfoliodemo.assets.models import Asset, CustomMetaData, FileType
 from eportfoliodemo.assets.forms import MetaDataForm
+from eportfoliodemo.settings import MEDIA_ROOT
+
+def ajax_create_asset(request):
+    if request.method == "POST":
+        file_type = request.FILES['file'].content_type.split('/')[1]
+        asset = Asset()
+        asset.author = request.user
+        asset.name = str.replace(str(request.FILES['file']), MEDIA_ROOT, '')
+        asset_type, created = FileType.objects.get_or_create(name=file_type)
+        asset.file = request.FILES['file']
+        asset.size = request.FILES['file'].size
+        asset.save()
+        asset.filetype.add(asset_type)
+        json_serializer = serializers.get_serializer("json")()
+        new_asset = json_serializer.serialize(Asset.objects.filter(id=asset.id))
+        return HttpResponse (new_asset, mimetype='application/json')
 
 def ajax_get_asset(request):
     requested_asset = request.GET['asset']
