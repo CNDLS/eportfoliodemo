6b12f11c4cb1d143344b1ac2329f69ec1ea4f5f8
Marie Selvanadin <bhindu20@gmail.com>
2011-10-11 16:45:24 -0400

    views to get items in a folder to update main section

--- folders/urls.py
+++ folders/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^create/$', 'folders.views.create', name="create_folder"),
     url(r'^(?P<folder_id>\d+)/update/$', 'folders.views.update', name="update_folder"),
     url(r'^(?P<folder_id>\d+)/rename/$', 'folders.views.ajax_rename_folder', name='ajax_rename_folder'),
-    url(r'^(?P<folder_id>\d+)/delete/$', 'folders.views.ajax_delete_folder', name='ajax_delete_folder')
+    url(r'^(?P<folder_id>\d+)/delete/$', 'folders.views.ajax_delete_folder', name='ajax_delete_folder'),
+    url(r'^get_folder_items/$', 'folders.views.ajax_get_folder_items', name='ajax_get_folder_items')
 )
