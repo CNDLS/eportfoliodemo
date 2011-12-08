4d6250a5d1bdd9dd603db40f90ba9774f46d8b57
Marie Selvanadin <bhindu20@gmail.com>
2011-12-08 11:13:52 -0500

    initial data for present

---
+++ present/sql/projecttype.sql
@@ -0,0 +1,5 @@
+INSERT INTO `present_projecttype` (`id`, `name`, `description`)
+VALUES
+	(1,'Website','Create a website for the project'),
+	(2,'Digital Story','Create a digital story for the project'),
+	(3,'Poster','Create a poster for the project');
---
+++ present/sql/template.sql
@@ -0,0 +1,4 @@
+INSERT INTO `present_template` (`id`, `name`, `description`, `created`, `modified`, `type_id`, `template_path`, `owner_id`, `is_default`, `screenshot`)
+VALUES
+	(1,'Basic Project Template','A basic template for a project','2011-12-08 10:41:50','2011-12-08 10:41:50',2,'basic/',1,0,''),
+	(2,'Basic Page Template','A very basic 2 column template for a page.','2011-12-08 10:42:37','2011-12-08 10:42:37',1,'basic/',1,1,'');
