0b594b33745e03b183308570aa90c1ed82dd4265
William Garr <wjg8@georgetown.edu>
2012-01-26 14:16:47 -0500

    backing out custom_include in present_tags.

--- present/templatetags/present_tags.py
+++ present/templatetags/present_tags.py
@@ -3,8 +3,8 @@ from django.template.defaultfilters import stringfilter
 from django.template import loader_tags, Template
 from django.conf import settings
 from django.template.loader import find_template
-from django.template.loader_tags import BaseIncludeNode
-from django.template.base import TemplateSyntaxError, TemplateDoesNotExist
+# from django.template.loader_tags import BaseIncludeNode
+# from django.template.base import TemplateSyntaxError, TemplateDoesNotExist
 
 register = template.Library()
 
@@ -24,70 +24,70 @@ def editable_state(path):
 # including compiled templates (for eventual uploadable templates)
 
 
-class CustomIncludeNode(BaseIncludeNode):
-	def __init__(self, template_name, template_dir, *args, **kwargs):
-		super(CustomIncludeNode, self).__init__(*args, **kwargs)
-		self.template_name = template_name
-		self.template_dir = template_dir
-
-	def render(self, context):
-		try:
-			template = self.get_template(context)
-			return self.render_template(template, context)
-		except:
-			if settings.TEMPLATE_DEBUG:
-				raise
-		return ''
-		
-	def get_template(self, context):
-		template, origin = find_template( self.template_name.resolve(context), [self.template_dir.resolve(context)] )
-		if not hasattr(template, 'render'):
-			# template needs to be compiled
-			template = get_template_from_string(template, origin, self.template_name)
-		return template
+# class CustomIncludeNode(BaseIncludeNode):
+#   def __init__(self, template_name, template_dir, *args, **kwargs):
+#       super(CustomIncludeNode, self).__init__(*args, **kwargs)
+#       self.template_name = template_name
+#       self.template_dir = template_dir
+# 
+#   def render(self, context):
+#       try:
+#           template = self.get_template(context)
+#           return self.render_template(template, context)
+#       except:
+#           if settings.TEMPLATE_DEBUG:
+#               raise
+#       return ''
+#       
+#   def get_template(self, context):
+#       template, origin = find_template( self.template_name.resolve(context), [self.template_dir.resolve(context)] )
+#       if not hasattr(template, 'render'):
+#           # template needs to be compiled
+#           template = get_template_from_string(template, origin, self.template_name)
+#       return template
             
 # slight change to default template tag which allows including a template we put in a different place.
-def do_custom_include(parser, token):
-    """
-    Loads a template and renders it with the current context. You can pass
-    additional context using keyword arguments.
-
-    Example::
-
-        {% include "foo/some_include" %}
-        {% include "foo/some_include" with bar="BAZZ!" baz="BING!" %}
-
-    Use the ``only`` argument to exclude the current context when rendering
-    the included template::
-
-        {% include "foo/some_include" only %}
-        {% include "foo/some_include" with bar="1" only %}
-    """
-    bits = token.split_contents()
-    if len(bits) < 3:
-        raise TemplateSyntaxError("%r tag takes at least two arguments: the name of the template to be included, and the directory in which to find it." % bits[0])
-    options = {}
-    remaining_bits = bits[3:]
-    while remaining_bits:
-        option = remaining_bits.pop(0)
-        if option in options:
-            raise TemplateSyntaxError('The %r option was specified more '
-                                      'than once.' % option)
-        if option == 'with':
-            value = token_kwargs(remaining_bits, parser, support_legacy=False)
-            if not value:
-                raise TemplateSyntaxError('"with" in %r tag needs at least '
-                                          'one keyword argument.' % bits[0])
-        elif option == 'only':
-            value = True
-        else:
-			value = False
-            # raise TemplateSyntaxError('Unknown argument for %r tag: %r.' %
-            #                           (bits[0], option))
-        options[option] = value
-    isolated_context = options.get('only', False)
-    namemap = options.get('with', {})
-    return CustomIncludeNode(parser.compile_filter(bits[1]), parser.compile_filter(bits[2]), extra_context=namemap,
-                       isolated_context=isolated_context)
-
-register.tag('custom_include', do_custom_include)
+# def do_custom_include(parser, token):
+#     """
+#     Loads a template and renders it with the current context. You can pass
+#     additional context using keyword arguments.
+# 
+#     Example::
+# 
+#         {% include "foo/some_include" %}
+#         {% include "foo/some_include" with bar="BAZZ!" baz="BING!" %}
+# 
+#     Use the ``only`` argument to exclude the current context when rendering
+#     the included template::
+# 
+#         {% include "foo/some_include" only %}
+#         {% include "foo/some_include" with bar="1" only %}
+#     """
+#     bits = token.split_contents()
+#     if len(bits) < 3:
+#         raise TemplateSyntaxError("%r tag takes at least two arguments: the name of the template to be included, and the directory in which to find it." % bits[0])
+#     options = {}
+#     remaining_bits = bits[3:]
+#     while remaining_bits:
+#         option = remaining_bits.pop(0)
+#         if option in options:
+#             raise TemplateSyntaxError('The %r option was specified more '
+#                                       'than once.' % option)
+#         if option == 'with':
+#             value = token_kwargs(remaining_bits, parser, support_legacy=False)
+#             if not value:
+#                 raise TemplateSyntaxError('"with" in %r tag needs at least '
+#                                           'one keyword argument.' % bits[0])
+#         elif option == 'only':
+#             value = True
+#         else:
+#           value = False
+#             # raise TemplateSyntaxError('Unknown argument for %r tag: %r.' %
+#             #                           (bits[0], option))
+#         options[option] = value
+#     isolated_context = options.get('only', False)
+#     namemap = options.get('with', {})
+#     return CustomIncludeNode(parser.compile_filter(bits[1]), parser.compile_filter(bits[2]), extra_context=namemap,
+#                        isolated_context=isolated_context)
+# 
+# register.tag('custom_include', do_custom_include)
