edcda76c536b911189600a8ecca17a2a06fdebc0
billgarr <cndls_developers@georgetown.edu>
2012-02-02 17:45:33 -0500

    indent errors.

--- usercollections/views.py
+++ usercollections/views.py
@@ -16,68 +16,68 @@ from eportfoliodemo.snippets.template import render_block_to_string
 def new(request):
    	if (request.user.is_authenticated()):
 		current_user = User.objects.get(pk=request.user.id)
-    collection_form = CollectionForm(instance=Collection(owner=current_user))
+   	collection_form = CollectionForm(instance=Collection(owner=current_user))
 
-    return render_to_response('usercollections/new.html', { 'collection_form': collection_form }, context_instance=RequestContext(request))
+   	return render_to_response('usercollections/new.html', { 'collection_form': collection_form }, context_instance=RequestContext(request))
 
 
     
 # requires request.method == "POST"
 def create(request):
     # make user_profile from post
-    collection_owner = User.objects.get(pk=request.POST.get("owner"))
-    collection = Collection(owner=collection_owner)
-    for field_name in collection._meta.get_all_field_names():
+   	collection_owner = User.objects.get(pk=request.POST.get("owner"))
+   	collection = Collection(owner=collection_owner)
+   	for field_name in collection._meta.get_all_field_names():
         # don't need primary key, user, or many-to-many associations
-        if request.POST.get(field_name) and (field_name != 'owner') and (field_name != 'parent'):
-            setattr(collection, field_name, request.POST.get(field_name))
+   	   	if request.POST.get(field_name) and (field_name != 'owner') and (field_name != 'parent'):
+   	   	   	setattr(collection, field_name, request.POST.get(field_name))
 
-    collection_form = CollectionForm(instance=collection)
+   	collection_form = CollectionForm(instance=collection)
 
     # if collection_form.is_valid():
-    collection.save()
+   	collection.save()
 
     # return HttpResponseRedirect(request.META['SCRIPT_NAME'] + '/library/' + str(collection.owner_id))
-    json_serializer = serializers.get_serializer("json")()
-    return HttpResponse (json_serializer.serialize([collection]), mimetype='application/json')
+   	json_serializer = serializers.get_serializer("json")()
+   	return HttpResponse (json_serializer.serialize([collection]), mimetype='application/json')
 
 
 
 # requires request.method == "POST"
 def update(request, collection_id):
     # make user_profile from post
-    collection_owner = User.objects.get(pk=request.POST.get("owner"))
-    collection = Collection.objects.get(pk=collection_id)
-    for field_name in collection._meta.get_all_field_names():
+   	collection_owner = User.objects.get(pk=request.POST.get("owner"))
+   	collection = Collection.objects.get(pk=collection_id)
+   	for field_name in collection._meta.get_all_field_names():
         # don't need primary key, user, or many-to-many associations
-        if request.POST.get(field_name) and (field_name != 'owner') and (field_name != 'parent'):
-            setattr(collection, field_name, request.POST.get(field_name))
+   	   	if request.POST.get(field_name) and (field_name != 'owner') and (field_name != 'parent'):
+   	   	   	setattr(collection, field_name, request.POST.get(field_name))
             
-    collection_form = CollectionForm(instance=collection)
+   	collection_form = CollectionForm(instance=collection)
 
     # if collection_form.is_valid():
-    collection.save()
+   	collection.save()
 
-    return HttpResponseRedirect(request.META['SCRIPT_NAME'] + '/library/' + str(collection.owner_id))
+   	return HttpResponseRedirect(request.META['SCRIPT_NAME'] + '/library/' + str(collection.owner_id))
 
 
 
 def ajax_rename_collection(request, collection_id):
-    if request.is_ajax():
-        try:
-            collection = Collection.objects.get(pk=collection_id)
-            collection.name = request.POST.get("name")
-            collection.save()
-            json_serializer = serializers.get_serializer("json")()
-            return HttpResponse (json_serializer.serialize([collection]), mimetype='application/json')
+   	if request.is_ajax():
+   	   	try:
+			collection = Collection.objects.get(pk=collection_id)
+			collection.name = request.POST.get("name")
+			collection.save()
+			json_serializer = serializers.get_serializer("json")()
+			return HttpResponse (json_serializer.serialize([collection]), mimetype='application/json')
 
-        except Exception as exception:
-            return HttpResponse(content=exception, status=500)
+			except Exception as exception:
+				return HttpResponse(content=exception, status=500)
 
 
 def ajax_delete_collection(request, collection_id):
-    if request.is_ajax():
-        collection = Collection.objects.get(pk=collection_id)
-        collection_owner_id = collection.owner_id
-        collection.delete()
-        return HttpResponseRedirect(reverse('collectionitems_index', args=[collection_owner_id]))
+	if request.is_ajax():
+		collection = Collection.objects.get(pk=collection_id)
+		collection_owner_id = collection.owner_id
+		collection.delete()
+		return HttpResponseRedirect(reverse('collectionitems_index', args=[collection_owner_id]))
